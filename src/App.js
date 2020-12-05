fd9ceffa2eaa5077552821e8214f846be65cee36
MehfoozurRehman <mehfoozijaz786@gmail.com>
2020-12-05 08:40:16 +0500

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect } from 'react';
 import Editor from './Editor';
-import useLocalStorage from './useLocalStorage'
+import useLocalStorage from './useLocalStorage';
+import './App.css';
 
 function App() {
   const [html, setHtml] = useLocalStorage('html','') 

2cc953f12942154d093c9d543bf48ce4fef56dd2
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2026-01-09 18:45:58 +0500

    chore: deps update & pnpm standardize

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig({
